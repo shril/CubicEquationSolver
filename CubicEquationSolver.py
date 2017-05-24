d2a64a6cbae2389e057cc73fb8d217eb12864576
Halder <Devojoyti.Halder@atkinsglobal.com>
2017-05-24 18:33:06 +0530

    testing

--- CubicEquationSolver.py
+++ CubicEquationSolver.py
@@ -24,44 +24,44 @@ import numpy as np
 # as parameters and it returns the roots in form of numpy array.
 # Polynomial Structure -> ax^3 + bx^2 + cx + d = 0
 
-def main(a, b, c, d):
+def solve(a, b, c, d):8=
 
     f = findF(a, b, c)                          # Helper Temporary Variable
     g = findG(a, b, c, d)                       # Helper Temporary Variable
     h = findH(g, f)                             # Helper Temporary Variable
 
     if f == 0 and g == 0 and h == 0:            # All 3 Roots are Real and Equal
-        if (d/a) >= 0:
-            x = (d/a) ** (1/3.0) * -1
+        if (d / a) >= 0:
+            x = (d / a) ** (1 / 3.0) * -1
         else:
             x = (-d / a) ** (1 / 3.0)
         return np.array([x, x, x])              # Returning Equal Roots as numpy array.
 
     elif h <= 0:                                # All 3 roots are Real
 
-        i = math.sqrt((g ** 2 / 4) - h)         # Helper Temporary Variable
+        i = math.sqrt(((g ** 2) / 4) - h)         # Helper Temporary Variable
         j = i ** (1 / 3.0)                      # Helper Temporary Variable
-        k = math.acos(-g / (2 * i))             # Helper Temporary Variable
+        k = math.acos(-(g / (2 * i)))             # Helper Temporary Variable
         L = j * -1                              # Helper Temporary Variable
         M = math.cos(k / 3.0)                   # Helper Temporary Variable
         N = math.sqrt(3) * math.sin(k / 3.0)    # Helper Temporary Variable
-        P = (b / a) * (1 / 3.0) * -1            # Helper Temporary Variable
+        P = (b / (3.0 * a)) * -1            # Helper Temporary Variable
 
-        x1 = 2 * j * math.cos(k/3.0) - (b/a)*(1/3.0)
+        x1 = 2 * j * math.cos(k / 3.0) - (b / a)*(1 / 3.0)
         x2 = L * (M + N) + P
         x3 = L * (M - N) + P
 
         return np.array([x1, x2, x3])           # Returning Real Roots as numpy array.
 
     elif h > 0:                                 # One Real Root and two Complex Roots
-        R = -(g/2.0) + math.sqrt(h)             # Helper Temporary Variable
+        R = -(g / 2.0) + math.sqrt(h)           # Helper Temporary Variable
         if R >= 0:
             S = R ** (1 / 3.0)                  # Helper Temporary Variable
         else:
             S = (-R) ** (1 / 3.0) * -1          # Helper Temporary Variable
-        T = -(g/2.0) - math.sqrt(h)
+        T = -(g / 2.0) - math.sqrt(h)
         if T >= 0:
-            U = (T ** (1/3.0))                  # Helper Temporary Variable
+            U = (T ** (1 / 3.0))                # Helper Temporary Variable
         else:
             U = ((-T) ** (1 / 3.0)) * -1        # Helper Temporary Variable
 
@@ -74,14 +74,14 @@ def main(a, b, c, d):
 
 # Helper function to return float value of f.
 def findF(a, b, c):
-    return ((3.0*c/a) - (b**2.0/a**2.0))/3.0
+    return ((3.0 * c / a) - ((b ** 2.0) / (a ** 2.0))) / 3.0
 
 
 # Helper function to return float value of g.
 def findG(a, b, c, d):
-    return (((2.0*b**3.0)/a**3.0) - ((9.0*b*c)/a**2.0) + (27.0*d/a))/27.0
+    return (((2.0 * (b ** 3.0)) / (a ** 3.0)) - ((9.0 * b * c) / (a **2.0)) + (27.0 * d / a)) /27.0
 
 
 # Helper function to return float value of h.
 def findH(g, f):
-    return ((g**2.0)/4.0 + (f**3.0)/27.0)
+    return ((g ** 2.0) / 4.0 + (f ** 3.0) / 27.0)
